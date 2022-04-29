6323ee9b74a87f0b79e7468fa8f07b7590900b95
clairecheng-94 <76176353+clairecheng-94@users.noreply.github.com>
2022-04-29 12:36:50 -0400

    Update mr-ieu-met.r

--- mr-ieu-met.r
+++ mr-ieu-met.r
@@ -5,10 +5,10 @@ library(data.table)
 #Read table 
 #Make sure names dictate which ones are exposure and outcome data 
 #outcome
-df <- fread('/home/cfc85413/PUFAS/ieu.out.tsv', header=TRUE, sep="\t")
+outcome_dat <- fread('/home/cfc85413/PUFAS/ieu.out.tsv', header=TRUE, sep="\t")
 
 #Omega 3 data table
-dv<- fread('/home/cfc85413/PUFAS/CConvertP2.out.tsv', header=TRUE, sep="\t") 
+exposure_dat<- fread('/home/cfc85413/PUFAS/CConvertP2.out.tsv', header=TRUE, sep="\t") 
 
 
 
@@ -24,10 +24,11 @@ exposireO3<-read_exposure_data(filename ='/home/cfc85413/PUFAS/CConvertP2.out.ts
   samplesize_col = 114999) 
   
 #Clump Data
+clump_data<-fread('/home/cfc85413/PUFAS/Omega_3_extract_exp_data_clump.txt', header=TRUE, sep="\t")
 
 
 #Used clump data in the read_outcome_data 
-outcome_dat <- read_outcome_data(snps = exposireO3$SNP,filename = '/home/cfc85413/PUFAS/ieu.out.tsv', sep = "\t" ,snp_col = "SNP",   
+outcome_dat <- read_outcome_data(snps = clump_data$SNP,filename = '/home/cfc85413/PUFAS/ieu.out.tsv', sep = "\t" ,snp_col = "SNP",   
   beta_col ="BETA",  
   se_col = "SE", 
   effect_allele_col = "ALT",
@@ -38,3 +39,22 @@ outcome_dat <- read_outcome_data(snps = exposireO3$SNP,filename = '/home/cfc8541
   
  #Harmonising the data
  res<-harmonise_data(exposireO3, outcome_dat)
+
+#sensitivity analysis 
+ mr_heterogeneity(res)
+
+mr_pleiotropy_test(res)
+res_loo<-mr_leaveoneout(res)
+head(res_loo)
+
+funnel<-mr_singlesnp(
+ res,
+ parameters = default_parameters(),
+ single_method = "mr_wald_ratio",
+ all_method = c("mr_ivw", "mr_egger_regression")
+ )
+
+mr_funnel_plot(funnel)
+
+
+
